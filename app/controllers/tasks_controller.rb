e154eca1289ecaa06bd8880c5440aa207933bd03
pipivybin <github email address>
2019-06-10 12:06:11 -0400

    added task controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ require './config/environment'
 
 class TasksController < ApplicationController
 
+get '/projects/:id/edit' do
+    if logged_in?
+        @user = current_user
+        @project = Project.find(params[:id])
+        erb :'/tasks/edit'
+    else
+        redirect '/login'
+    end
+end
+
+patch '/projects/:id/edit' do
+    project = Project.find(params[:id])
+    project.update(params[:project])
+    Task.update(params[:tasks].keys, params[:tasks].values) 
+    if !params[:new][:name].empty?
+        project.tasks.create(params[:new])
+    end
+    redirect "/projects/#{params[:id]}"   
+end
+
 end
